323fc78c4addd91d68d50b5946b49f84d63f8588
iwat <iwatth@gmail.com>
2017-09-08 10:49:57 +0700

    Initial commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+~/Library/Application\ Support/Ethereum\ Wallet/binaries/Geth/unpacked/geth \
+  --dev \
+  --preload boot.js \
+  --etherbase 0x2f93db485bd163c6fc61ef748eea34c0e35f49c6 \
+  --datadir db \
+  console
