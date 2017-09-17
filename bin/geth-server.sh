94f6bc2c100cc15283afa24f2beeb103e9cd9d75
iwat <iwatth@gmail.com>
2017-09-17 13:40:21 +0700

    Use global geth

--- bin/geth-server.sh
+++ bin/geth-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-~/Library/Application\ Support/Ethereum\ Wallet/binaries/Geth/unpacked/geth \
+geth \
   --dev \
   --preload $DIR/../ethereum/boot.js \
   --etherbase 0x2f93db485bd163c6fc61ef748eea34c0e35f49c6 \
