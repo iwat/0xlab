395af81240c72faf089f24559107e953e0655d09
iwat <iwatth@gmail.com>
2017-09-17 13:38:20 +0700

    Reorg for Node Express

--- bin/geth-server.sh
+++ bin/geth-server.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 ~/Library/Application\ Support/Ethereum\ Wallet/binaries/Geth/unpacked/geth \
   --dev \
-  --preload boot.js \
+  --preload $DIR/../ethereum/boot.js \
   --etherbase 0x2f93db485bd163c6fc61ef748eea34c0e35f49c6 \
-  --datadir db \
+  --datadir $DIR/../ethereum/db \
+  --jspath $DIR/../ethereum/ \
   --rpc \
   --networkid 1337 \
   console
